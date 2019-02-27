1bc7380d3729b3d93ccd690d9972f0096ce9ae7e
lishangting <1053611095@qq.com>
2019-02-27 17:22:38 +0800

    初始化模块

--- src/main/java/cn/lstfight/microservice/MicroServiceApplication.java
+++ src/main/java/cn/lstfight/microservice/MicroServiceApplication.java
@@ -1,14 +1,23 @@
 package cn.lstfight.microservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 李尚庭
+ * @date 2019-2-27
+ */
 @SpringBootApplication
-public class MicroServiceApplication {
+public class MicroServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         //扫描入口是MicroServiceApplication.class会扫描到@SpringBootApplication注解
         SpringApplication.run(MicroServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
